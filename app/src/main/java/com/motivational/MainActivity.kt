ae1665c7f9e4820a46f8045c2fea9756463105a7
Luis Filipe Lobo <luisfilipelobo.dev@gmail.com>
2023-02-16 18:47:49 -0300

    Ajustes na tela principal

--- app/src/main/java/com/motivational/MainActivity.kt
+++ app/src/main/java/com/motivational/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.motivational
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.motivational.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Expandir o layout para ter acesso aos elementos de interface no código
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        // Esconder support action bar
+        supportActionBar?.hide()
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+
     }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_new_phrase) {
+            TODO("Falta implementar")
+        }
+    }
+
 }
